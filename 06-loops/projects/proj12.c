1aa58f9bad195b607209ad61b3abe492128fd988
k-hn <125813796+k-hn@users.noreply.github.com>
2024-01-10 10:21:50 +0000

    update cahpter6-project 12

--- 06-loops/projects/proj12.c
+++ 06-loops/projects/proj12.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 
 int main(void) {
-  int n;
-  float e = 1.0f, fact = 1.0f, epsilon;
+  int fact = 1;
+  float e = 1.0f, current_term = 1.0f, epsilon;
 
   printf("Enter episilon: ");
   scanf("%f", &epsilon);
 
-  for (int i = 1; epsilon > e; i++) {
-    fact *= i;
-    e += 1.0f / fact;
+  for (int i = 1; current_term > epsilon; i++) {
+    current_term = 1.0f / (fact *= i);
+    e += current_term;
   }
 
   printf("e approximate: %f\n", e);
 
   return EXIT_SUCCESS;
 }
-
-// TODO: can't figure out loop condition yet
